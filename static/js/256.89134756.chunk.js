24e846d6bf431cd3a27c9980b82c6775a2863453
Serhii-Fesianov <143389508+Serhii-Fesianov@users.noreply.github.com>
2024-01-23 14:19:06 +0000

    Deploying to gh-pages from @ Serhii-Fesianov/goit-react-hw-05-movies@d7d0474b99fe654d8ad3682321eac7803de1d2b9 🚀

---
+++ static/js/256.89134756.chunk.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkreact_homework_template=self.webpackChunkreact_homework_template||[]).push([[256],{256:function(e,r,t){t.r(r);t(791);var n=t(87),o=t(184);r.default=function(){return(0,o.jsxs)("div",{children:[(0,o.jsx)("h1",{children:"Sorry... page is not found"}),(0,o.jsx)(n.rU,{to:"/",children:"Go home"})]})}}}]);
+//# sourceMappingURL=256.89134756.chunk.js.map
